3d25f1d22289bac72fbf4b63db7bb76f05244272
MikhailoVL <51120969+MikhailoVL@users.noreply.github.com>
2019-07-14 13:02:12 +0300

    Update NOD.py

--- NOD.py
+++ NOD.py
@@ -12,24 +12,19 @@ def myNod_first(first_i, second_i):
 			for i in range(first_i):
 			#Проверяем делится ли оба числа  на одно и тоже число 
 				if (first_i % (first_i - i)) == 0 and (second_i %(first_i - i)) == 0 :
-					print("Второй вход  = Общий делитель = " + str(first_i - i))
-					print (i)
-					print("Второй вход  = Общий делитель = " + str(first_i - i))
 					return first_i - i
 	else:
 		if first_i%second_i == 0 :
-			print("Третий вход  = Общий делитель =" + str(second_i))
+			print("Общий делитель =" + str(second_i))
 			return second_i
 		else:
 			#Создается счетчик перебирающий все цифры в загаданном числе от большего к меньшему
 			for i in range(second_i):
 				#Проверяем делится ли оба числа  на одно и тоже число 
 				if (first_i % (second_i - i)) == 0 and (second_i %(second_i - i)) == 0 :
-					print("Четвертый вход = Общий делитель = " + str(second_i - i))
+					print("Общий делитель = " + str(second_i - i))
 					return second_i - i
 
-print("Это программа вычисляет средний делитель 3-мя способами \n")
-print("Первый способ опредялем общий делитель путем перебора цифр в числе")
 # Генерируем первое случайное число 
 first_int =int(random.randint(1, 1000000))
 # Генерируем второе случайное число 
